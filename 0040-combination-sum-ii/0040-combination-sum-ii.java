fcaa8fc34bb98fbb6afdf5f56222ca641e633a81
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-01-09 23:39:53 +0530

    Time: 6 ms (71.1%), Space: 45.2 MB (53.62%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);  
+        List<List<Integer>> ans = new ArrayList<>();  
+        List<Integer> ds = new ArrayList<>();  
+        findCombination(0, target, candidates, ans, ds); 
+        return ans; 
+    }
+     public void findCombination(int ind, int target, int[] arr, List<List<Integer>> ans, List<Integer> ds) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(ds));  
+            return;
+        }
+
+        for (int i = ind; i < arr.length; i++) {
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+            if (arr[i] > target) break;
+
+            ds.add(arr[i]);
+
+            findCombination(i + 1, target - arr[i], arr, ans, ds);
+
+            ds.remove(ds.size() - 1);
+        }
+     }
+}
