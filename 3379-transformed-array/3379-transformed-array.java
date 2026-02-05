b6195521b63802a98185f157a54d97797622a50b
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-02-05 19:14:36 +0530

    Time: 1 ms (98.72%), Space: 46.3 MB (96.47%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -3,13 +3,7 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                ans[i] = nums[(i+nums[i])%n];
-            } else if (nums[i] < 0) {
-                ans[i] = nums[(i+nums[i]%n+n)%n];
-            } else {
-                ans[i] = nums[i];
-            }
+             ans[i] = nums[(i+nums[i]%n+n)%n];
         }
         return ans;
     }
