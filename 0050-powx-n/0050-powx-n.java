bea1ca2e3626b68c29272b00b143d1e5680ab05c
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-02-25 11:56:02 +0530

    Time: 0 ms (100%), Space: 48.2 MB (47.8%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,8 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        return solve(x,n);
-    }
-    private double solve(double x,int n){
-        return Math.pow(x,n);
+        double ans=1.0;
+        long res=n;
+        if(res<0)res=(-1)*res;
+        while(res>0){
+            if(res%2!=0){
+                ans=ans*x;
+                res=res-1;
+            }else{
+                x=x*x;
+                res=res/2;
+            }
+        }
+        if(n<0)ans=(double)(1.0)/(double)(ans);
+        return ans;
     }
 }
