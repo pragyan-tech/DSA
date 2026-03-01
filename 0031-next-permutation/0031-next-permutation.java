c7d70f3d0af6635295c8d473e52c8c008011f541
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-03-01 20:17:12 +0530

    Time: 0 ms (100%), Space: 44.3 MB (98.26%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,7 +1,7 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i=nums.length-2;
-        while( i>=0 && nums[i]>=nums[i+1]){
+        while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
         if(i>=0){
@@ -9,21 +9,20 @@ class Solution {
             while(nums[j]<=nums[i]){
                 j--;
             }
-            swap(nums,i,j);
+            swap(i,j,nums);
         }
         rev(nums,i+1);
-
     }
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int a,int b,int[]nums){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
-    private void rev(int[] arr, int start) {
-        int i = start;
-        int j = arr.length - 1;
-        while (i < j) {
-            swap(arr, i, j);
+    public void rev(int[]arr,int start){
+        int i=start;
+        int j=arr.length-1;
+        while(i<j){
+            swap(i,j,arr);
             i++;
             j--;
         }
