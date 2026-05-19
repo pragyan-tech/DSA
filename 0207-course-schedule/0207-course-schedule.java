15ec8faa6b5dce1dc967f644fcd2e2e5af840097
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-05-19 13:21:20 +0530

    Time: 90 ms (5.35%), Space: 47 MB (62.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,37 +1,35 @@
 class Solution {
-    public boolean dfs(int node, int[] vis, List<List<Integer>> adj) {
-        vis[node] = 1;
-
-        for (int neighbor : adj.get(node)) {
-            if (vis[neighbor] == 1) {
-                return true; 
+    public boolean dfs(int node,int[]vis,List<List<Integer>>adj){
+        vis[node]=1;
+        for(int i:adj.get(node)){
+            if(vis[i]==1){
+                return true;
             }
-            if (vis[neighbor] == 0) {
-                if (dfs(neighbor, vis, adj)) return true;
+            if(vis[i]==0){
+                if(dfs(i,vis,adj)){
+                return true;
+                }
             }
         }
-
-        vis[node] = 2; 
+        vis[node]=2;
         return false;
     }
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
+        List<List<Integer>>adj=new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for (int[] pre : prerequisites) {
-            adj.get(pre[1]).add(pre[0]); 
+        for(int[]arr:prerequisites){
+            adj.get(arr[1]).add(arr[0]);
         }
+        int vis[]=new int[numCourses];
 
-        int[] vis = new int[numCourses];
-        
-        for (int i = 0; i < numCourses; i++) {
-            if (vis[i] == 0) {
-                if (dfs(i, vis, adj)) return false; 
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){
+                if(dfs(i,vis,adj))return false;
             }
         }
-        
-        return true; 
+        return true;
     }
 }
