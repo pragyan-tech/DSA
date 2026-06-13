106f58709781d60341d66eb46f98b7bc7034239e
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-06-13 19:33:13 +0530

    Time: 2 ms (99.19%), Space: 44.2 MB (84.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        int right= s.length()-1;
+        while(left<right){
+            char first=s.charAt(left);
+            char last=s.charAt(right);
+            
+            if(!Character.isLetterOrDigit(first)){
+                left++;
+            }else if(!Character.isLetterOrDigit(last)){
+                right--;
+            }else{
+                if(Character.toLowerCase(first)!=Character.toLowerCase(last)){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
