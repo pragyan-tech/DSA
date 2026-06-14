2575c80ef81596496311a3e38a011b734cf5ba77
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-06-14 19:26:17 +0530

    Time: 4 ms (67.03%), Space: 45.4 MB (89.42%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l = 0, r = 0, cnt = 0;
-        int minLen = Integer.MAX_VALUE;
-        int start = -1;
-        int n = s.length();
-        int m = t.length();
-        int[] hash = new int[256];
+        int l=0, r=0, cnt=0;
+        int start=-1;
+        int minLen=Integer.MAX_VALUE;
+        int []hash=new int[256];
+        int n=s.length();
+        int m=t.length();
 
-        for (int i = 0; i < m; i++) {
+        for(int i=0;i<m;i++){
             hash[t.charAt(i)]++;
         }
 
-        while (r < n) {
-            if (hash[s.charAt(r)] > 0) {
+        while(r<n){
+            if(hash[s.charAt(r)]>0){
                 cnt++;
             }
             hash[s.charAt(r)]--;
 
-            while (cnt == m) {
-                if ((r - l + 1) < minLen) {
-                    minLen = r - l + 1;
-                    start = l;
+            while(cnt==m){
+                if(r-l+1 < minLen){
+                    minLen=r - l + 1;
+                    start=l;
                 }
                 hash[s.charAt(l)]++;
-                if (hash[s.charAt(l)] > 0) {
+                if(hash[s.charAt(l)]>0){
                     cnt--;
                 }
                 l++;
             }
             r++;
         }
-
-        return start == -1 ? "" : s.substring(start, start + minLen);
-    }
+        if(start==-1){
+            return "";
+        }else{
+            return s.substring(start, start+ minLen);
+        }
+}
 }
