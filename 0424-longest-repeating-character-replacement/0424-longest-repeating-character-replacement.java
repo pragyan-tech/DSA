58c50955a40dc16d50c6eb8e58ff3ca524cfd663
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-06-14 19:53:25 +0530

    Time: 8 ms (68.23%), Space: 45.8 MB (97.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,21 +1,20 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] count = new int[26];
-        int maxLen = 0;
-        int maxFreq = 0; 
-        int l = 0;
+        int l=0;
+        int maxLen=0;
+        int maxFreq=0;
+        int[]hash=new int[26];
 
-        for (int r = 0; r < s.length(); r++) {
-            count[s.charAt(r) - 'A']++;
-            maxFreq = Math.max(maxFreq, count[s.charAt(r) - 'A']);
+        for(int r=0; r < s.length(); r++){
+            hash[s.charAt(r)-'A']++;
+            maxFreq=Math.max(maxFreq, hash[s.charAt(r)-'A']);
 
-            while ((r - l + 1) - maxFreq > k) {
-                count[s.charAt(l) - 'A']--;
+            while((r-l+1)-maxFreq > k){
+                hash[s.charAt(l)-'A']--;
                 l++;
             }
-            maxLen = Math.max(maxLen, r - l + 1);
+            maxLen=Math.max(maxLen, r-l+1);
         }
-
         return maxLen;
     }
 }
