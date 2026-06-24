6ea991a8889c18a2dec0df3ed75a860be7443a8c
Pragyan Oza <149198774+pragyan-tech@users.noreply.github.com>
2026-06-24 12:20:53 +0530

    Time: 0 ms (100%), Space: 45.8 MB (5.36%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,15 +14,16 @@
  * }
  */
 class Solution {
-    public boolean helper(TreeNode root,Long min,Long max){
-        if (root==null)return true;
-        if((min!=null && root.val<=min)||(max!=null && root.val>=max)){
+    public boolean isValidBST(TreeNode root) {
+        return solve(root,null,null);
+    }
+    public boolean solve(TreeNode root, Long min, Long max){
+        if(root==null)return true;
+
+        if((min!=null && root.val<=min) || (max!=null && root.val>=max)){
             return false;
         }else{
-            return helper(root.left,min,(long)root.val)&& helper(root.right,(long)root.val,max);
+            return solve(root.left,min,(long)root.val) && solve(root.right,(long)root.val,max);
         }
-    }
-    public boolean isValidBST(TreeNode root) {
-        return helper(root,null,null);
     }
 }
